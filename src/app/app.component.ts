a9383db444f9497fda2ddf6fd161d17d3d48f8c2
Odessa Chesneau <odessa.chesneau@gmail.com>
2021-08-18 16:04:59 +0200

    Add http request helper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpRequestService} from "./helpers/http-request/http-request.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'api-eventdrive';
+export class AppComponent implements OnInit {
+    title = 'api-eventdrive';
+
+    constructor(private httpRequestService: HttpRequestService) {
+    }
+
+    ngOnInit(): void {
+        this.init().then();
+    }
+
+    async init(){
+        const result = await this.httpRequestService.sendGetRequest("https://api.dev.eventdrive.com/public/v1/events");
+        console.log(result);
+    }
 }
---
+++ src/app/helpers/http-request/http-request.service.ts
@@ -0,0 +1,52 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class HttpRequestService {
+
+    constructor() {
+    }
+
+    async sendGetRequest(url: string, token?: string): Promise<any> {
+        const response = await fetch(url, {
+            headers: {
+                'Authorization': token === undefined ? '' : token
+            }
+        });
+        return response.json();
+    }
+
+    async sendPostRequest(url: string, body: any, token?: string): Promise<any> {
+        const response = await fetch(url, {
+            method: 'POST',
+            body,
+            headers: {
+                'Authorization': token === undefined ? '' : token
+            }
+        });
+        return response.json();
+    }
+
+    async sendPutRequest(url: string, body: any, token?: string): Promise<any> {
+        const response = await fetch(url, {
+            method: 'PUT',
+            body,
+            headers: {
+                'Authorization': token === undefined ? '' : token
+            }
+        });
+        return response.json();
+    }
+
+    async sendDeleteRequest(url: string, body: any, token?: string): Promise<any> {
+        const response = await fetch(url, {
+            method: 'DELETE',
+            body,
+            headers: {
+                'Authorization': token === undefined ? '' : token
+            }
+        });
+        return response.json();
+    }
+}
